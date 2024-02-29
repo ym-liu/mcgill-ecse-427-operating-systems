f1bd086fbfc86cfa829a317746ca3fc6e3c395a6
ym-liu <ymliuy@gmail.com>
2024-02-29 13:45:11 -0500

    feat: add empty backing store

--- a2/interpreter.c
+++ a2/interpreter.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <stdbool.h>
@@ -13,7 +13,7 @@
 
 int MAX_ARGS_SIZE = 7;
 
-char* error_msgs[] = {
+char *error_msgs[] = {
 	"no error",
 	"file does not exist",
 	"file could not be loaded",
@@ -25,61 +25,68 @@ char* error_msgs[] = {
 	"non-alphanumeric token",
 	"unknown name",
 	"cd",
-	"mkdir"
-};
+	"mkdir"};
 
-int handle_error(enum Error error_code){
+int handle_error(enum Error error_code)
+{
 	printf("Bad command: %s\n", error_msgs[error_code]);
 	return error_code;
 }
 
 int help();
 int quit();
-int set(char* var, char* value);
-int print(char* var);
-int run(char* script);
-int echo(char* var);
+int set(char *var, char *value);
+int print(char *var);
+int run(char *script);
+int echo(char *var);
 int my_ls();
-int my_mkdir(char* dirname);
-int my_touch(char* filename);
-int my_cd(char* dirname);
+int my_mkdir(char *dirname);
+int my_touch(char *filename);
+int my_cd(char *dirname);
 int exec(char *fname1, char *fname2, char *fname3); //, char* policy, bool background, bool mt);
 
 // Interpret commands and their arguments
-int interpreter(char* command_args[], int args_size){
+int interpreter(char *command_args[], int args_size)
+{
 	if (args_size < 1)
 		return handle_error(TOO_FEW_TOKENS);
 	if (args_size > MAX_ARGS_SIZE)
 		return handle_error(TOO_MANY_TOKENS);
 
-	for (int i=0; i<args_size; i++)
+	for (int i = 0; i < args_size; i++)
 	{ // strip spaces, newline, etc.
 		command_args[i][strcspn(command_args[i], "\r\n")] = 0;
 	}
 
-	if (strcmp(command_args[0], "help")==0)
+	if (strcmp(command_args[0], "help") == 0)
 	{ // help
-	    if (args_size > 1) return handle_error(TOO_MANY_TOKENS);
-	    return help();
+		if (args_size > 1)
+			return handle_error(TOO_MANY_TOKENS);
+		return help();
 	}
 	else if (strcmp(command_args[0], "quit") == 0 || strcmp(command_args[0], "exit") == 0)
 	{ // quit
-		if (args_size > 1) return handle_error(TOO_MANY_TOKENS);
+		if (args_size > 1)
+			return handle_error(TOO_MANY_TOKENS);
 		// TODO
 		return quit();
 	}
-	else if (strcmp(command_args[0], "set")==0)
-	{ //set
-		if (args_size < 3) return handle_error(TOO_FEW_TOKENS);
+	else if (strcmp(command_args[0], "set") == 0)
+	{ // set
+		if (args_size < 3)
+			return handle_error(TOO_FEW_TOKENS);
 		int total_len = 0;
-		for(int i=2; i<args_size; i++){
-			total_len+=strlen(command_args[i])+1;
+		for (int i = 2; i < args_size; i++)
+		{
+			total_len += strlen(command_args[i]) + 1;
 		}
-		char *value = (char*) calloc(1, total_len);
+		char *value = (char *)calloc(1, total_len);
 		char spaceChar = ' ';
-		for(int i = 2; i < args_size; i++){
+		for (int i = 2; i < args_size; i++)
+		{
 			strncat(value, command_args[i], strlen(command_args[i]));
-			if(i < args_size-1){
+			if (i < args_size - 1)
+			{
 				strncat(value, &spaceChar, 1);
 			}
 		}
@@ -89,57 +96,69 @@ int interpreter(char* command_args[], int args_size){
 	}
 	else if (strcmp(command_args[0], "print") == 0)
 	{ // print
-		if (args_size < 2) return handle_error(TOO_FEW_TOKENS);
-		if (args_size > 2) return handle_error(TOO_MANY_TOKENS);
+		if (args_size < 2)
+			return handle_error(TOO_FEW_TOKENS);
+		if (args_size > 2)
+			return handle_error(TOO_MANY_TOKENS);
 		return print(command_args[1]);
 	}
 	else if (strcmp(command_args[0], "run") == 0)
 	{ // run
-		if (args_size < 2) return handle_error(TOO_FEW_TOKENS);
-		if (args_size > 2) return handle_error(TOO_MANY_TOKENS);	
+		if (args_size < 2)
+			return handle_error(TOO_FEW_TOKENS);
+		if (args_size > 2)
+			return handle_error(TOO_MANY_TOKENS);
 		return run(command_args[1]);
 	}
 	else if (strcmp(command_args[0], "echo") == 0)
 	{ // echo
-		if (args_size > 2) return handle_error(TOO_MANY_TOKENS);
+		if (args_size > 2)
+			return handle_error(TOO_MANY_TOKENS);
 		return echo(command_args[1]);
 	}
 	else if (strcmp(command_args[0], "my_ls") == 0)
 	{ // ls
-		if (args_size > 1) return handle_error(TOO_MANY_TOKENS);
+		if (args_size > 1)
+			return handle_error(TOO_MANY_TOKENS);
 		return my_ls(command_args[0]);
 	}
-	else if (strcmp(command_args[0], "my_mkdir")==0)
+	else if (strcmp(command_args[0], "my_mkdir") == 0)
 	{
-		if (args_size > 2) return handle_error(TOO_MANY_TOKENS);
+		if (args_size > 2)
+			return handle_error(TOO_MANY_TOKENS);
 		return my_mkdir(command_args[1]);
 	}
-	else if (strcmp(command_args[0], "my_touch")==0)
+	else if (strcmp(command_args[0], "my_touch") == 0)
 	{
-		if (args_size > 2) return handle_error(TOO_MANY_TOKENS);
+		if (args_size > 2)
+			return handle_error(TOO_MANY_TOKENS);
 		return my_touch(command_args[1]);
 	}
-	else if (strcmp(command_args[0], "my_cd")==0)
+	else if (strcmp(command_args[0], "my_cd") == 0)
 	{
-		if (args_size > 2) return handle_error(TOO_MANY_TOKENS);
-		return my_cd(command_args[1]);	
-	} 
-	else if (strcmp(command_args[0], "exec")==0)
-	{
-		if (args_size <= 1) return handle_error(TOO_FEW_TOKENS);
-		if (args_size > 5) return handle_error(TOO_MANY_TOKENS);
-		if(args_size == 2)
-			return exec(command_args[1],NULL,NULL);
-		else if(args_size == 3)
-			return exec(command_args[1],command_args[2],NULL); 
-		else if(args_size == 4)
-			return exec(command_args[1],command_args[2],command_args[3]);
+		if (args_size > 2)
+			return handle_error(TOO_MANY_TOKENS);
+		return my_cd(command_args[1]);
 	}
-	
+	else if (strcmp(command_args[0], "exec") == 0)
+	{
+		if (args_size <= 1)
+			return handle_error(TOO_FEW_TOKENS);
+		if (args_size > 5)
+			return handle_error(TOO_MANY_TOKENS);
+		if (args_size == 2)
+			return exec(command_args[1], NULL, NULL);
+		else if (args_size == 3)
+			return exec(command_args[1], command_args[2], NULL);
+		else if (args_size == 4)
+			return exec(command_args[1], command_args[2], command_args[3]);
+	}
+
 	return handle_error(BAD_COMMAND);
 }
 
-int help(){
+int help()
+{
 
 	char help_string[] = "COMMAND			DESCRIPTION\n \
 help			Displays all the commands\n \
@@ -151,13 +170,19 @@ run SCRIPT.TXT		Executes the file SCRIPT.TXT\n ";
 	return 0;
 }
 
-int quit(){
+int quit()
+{
 	printf("%s\n", "Bye!");
 	ready_queue_destory();
+
+	// remove backing store
+	system("rm -rf ./backing_store");
+
 	exit(0);
 }
 
-int set(char* var, char* value){
+int set(char *var, char *value)
+{
 	char *link = "=";
 	char buffer[1000];
 	strcpy(buffer, var);
@@ -167,35 +192,44 @@ int set(char* var, char* value){
 	return 0;
 }
 
-int print(char* var){
+int print(char *var)
+{
 	char *value = mem_get_value(var);
-	if(value == NULL) value = "\n";
-	printf("%s\n", value); 
+	if (value == NULL)
+		value = "\n";
+	printf("%s\n", value);
 	return 0;
 }
 
-int echo(char* var){
-	if(var[0] == '$') print(++var);
-	else printf("%s\n", var); 
-	return 0; 
+int echo(char *var)
+{
+	if (var[0] == '$')
+		print(++var);
+	else
+		printf("%s\n", var);
+	return 0;
 }
 
-int my_ls(){
+int my_ls()
+{
 	int errCode = system("ls | sort");
 	return errCode;
 }
 
-int my_mkdir(char *dirname){
+int my_mkdir(char *dirname)
+{
 	char *dir = dirname;
-	if(dirname[0] == '$'){
+	if (dirname[0] == '$')
+	{
 		char *value = mem_get_value(++dirname);
-		if(value == NULL || strchr(value, ' ') != NULL){
+		if (value == NULL || strchr(value, ' ') != NULL)
+		{
 			return handle_error(ERROR_MKDIR);
 		}
 		dir = value;
 	}
 	int namelen = strlen(dir);
-	char* command = (char*) calloc(1, 7+namelen); 
+	char *command = (char *)calloc(1, 7 + namelen);
 	strncat(command, "mkdir ", 7);
 	strncat(command, dir, namelen);
 	int errCode = system(command);
@@ -203,9 +237,10 @@ int my_mkdir(char *dirname){
 	return errCode;
 }
 
-int my_touch(char* filename){
+int my_touch(char *filename)
+{
 	int namelen = strlen(filename);
-	char* command = (char*) calloc(1, 7+namelen); 
+	char *command = (char *)calloc(1, 7 + namelen);
 	strncat(command, "touch ", 7);
 	strncat(command, filename, namelen);
 	int errCode = system(command);
@@ -213,52 +248,63 @@ int my_touch(char* filename){
 	return errCode;
 }
 
-int my_cd(char* dirname){
+int my_cd(char *dirname)
+{
 	struct stat info;
-	if(stat(dirname, &info) == 0 && S_ISDIR(info.st_mode)) {
-		//the object with dirname must exist and is a directory
+	if (stat(dirname, &info) == 0 && S_ISDIR(info.st_mode))
+	{
+		// the object with dirname must exist and is a directory
 		int errCode = chdir(dirname);
 		return errCode;
 	}
 	return handle_error(ERROR_CD);
 }
 
-
-int run(char* script){
-	//errCode 11: bad command file does not exist
+int run(char *script)
+{
+	// errCode 11: bad command file does not exist
 	int errCode = 0;
-	//load script into shell
+	// load script into shell
 	errCode = process_initialize(script);
-	if(errCode == 11){
+	if (errCode == 11)
+	{
 		return handle_error(errCode);
 	}
-	//run with FCFS
+	// run with FCFS
 	schedule_by_policy("FCFS"); //, false);
 	return errCode;
 }
 
-int exec(char *fname1, char *fname2, char *fname3) {
+int exec(char *fname1, char *fname2, char *fname3)
+{
 	int error_code = 0;
-	if(fname1 != NULL){
-        error_code = process_initialize(fname1);
-		if(error_code != 0){
+	if (fname1 != NULL)
+	{
+		error_code = process_initialize(fname1);
+		if (error_code != 0)
+		{
 			return handle_error(error_code);
 		}
-    }
-    if(fname2 != NULL){
-        error_code = process_initialize(fname2);
-		if(error_code != 0){
+	}
+	if (fname2 != NULL)
+	{
+		error_code = process_initialize(fname2);
+		if (error_code != 0)
+		{
 			return handle_error(error_code);
 		}
-    }
-    if(fname3 != NULL){
-        error_code = process_initialize(fname3);
-		if(error_code != 0){
+	}
+	if (fname3 != NULL)
+	{
+		error_code = process_initialize(fname3);
+		if (error_code != 0)
+		{
 			return handle_error(error_code);
 		}
-    } 
+	}
 	error_code = schedule_by_policy("RR");
-	if(error_code != 0){
+	if (error_code != 0)
+	{
 		return handle_error(error_code);
 	}
 }
--- a2/shell.c
+++ a2/shell.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
 #include <unistd.h>
 #include "interpreter.h"
 #include "shellmemory.h"
@@ -11,68 +11,89 @@
 int MAX_USER_INPUT = 1000;
 int parseInput(char ui[]);
 
-int main(int argc, char *argv[]) {
-	printf("%s\n", "Shell v2.0\n");
+int main(int argc, char *argv[])
+{
+    // CREATE EMPTY BACKING STORE DIRECTORY
+    // if already present, remove backing store
+    system("rm -rf ./backing_store");
+    // create backing store inside current directory
+    system("mkdir ./backing_store");
 
-	char prompt = '$';  				// Shell prompt
-	char userInput[MAX_USER_INPUT];		// user's input stored here
-	int errorCode = 0;					// zero means no error, default
+    printf("%s\n", "Shell v2.0\n");
 
-	//init user input
-	for (int i=0; i<MAX_USER_INPUT; i++)
-		userInput[i] = '\0';
-	
-	//init shell memory
-	mem_init();
+    char prompt = '$';              // Shell prompt
+    char userInput[MAX_USER_INPUT]; // user's input stored here
+    int errorCode = 0;              // zero means no error, default
 
-	while(1) {						
-        if (isatty(fileno(stdin))) printf("%c ",prompt);
+    // init user input
+    for (int i = 0; i < MAX_USER_INPUT; i++)
+        userInput[i] = '\0';
 
-		char *str = fgets(userInput, MAX_USER_INPUT-1, stdin);
-        if (feof(stdin)){
+    // init shell memory
+    mem_init();
+
+    while (1)
+    {
+        if (isatty(fileno(stdin)))
+            printf("%c ", prompt);
+
+        char *str = fgets(userInput, MAX_USER_INPUT - 1, stdin);
+        if (feof(stdin))
+        {
             freopen("/dev/tty", "r", stdin);
         }
 
-		if(strlen(userInput) > 0) {
+        if (strlen(userInput) > 0)
+        {
             errorCode = parseInput(userInput);
-            if (errorCode == -1) exit(99);	// ignore all other errors
+            if (errorCode == -1)
+                exit(99); // ignore all other errors
             memset(userInput, 0, sizeof(userInput));
-		}
-	}
+        }
+    }
 
-	return 0;
-
+    return 0;
 }
 
-int parseInput(char *ui) {
+int parseInput(char *ui)
+{
     char tmp[200];
     char *words[100];
-	memset(words, 0, sizeof(words));
-	
+    memset(words, 0, sizeof(words));
+
     int a = 0;
-    int b;                            
-    int w=0; // wordID    
+    int b;
+    int w = 0; // wordID
     int errorCode;
-    for(a=0; ui[a]==' ' && a<1000; a++);        // skip white spaces
-    
-    while(a<1000 && a<strlen(ui) && ui[a] != '\n' && ui[a] != '\0') {
-        while(ui[a]==' ') a++;
-        if(ui[a] == '\0') break;
-        for(b=0; ui[a]!=';' && ui[a]!='\0' && ui[a]!='\n' && ui[a]!=' ' && a<1000; a++, b++) tmp[b] = ui[a];
+    for (a = 0; ui[a] == ' ' && a < 1000; a++)
+        ; // skip white spaces
+
+    while (a < 1000 && a < strlen(ui) && ui[a] != '\n' && ui[a] != '\0')
+    {
+        while (ui[a] == ' ')
+            a++;
+        if (ui[a] == '\0')
+            break;
+        for (b = 0; ui[a] != ';' && ui[a] != '\0' && ui[a] != '\n' && ui[a] != ' ' && a < 1000; a++, b++)
+            tmp[b] = ui[a];
         tmp[b] = '\0';
-        if(strlen(tmp)==0) continue;
+        if (strlen(tmp) == 0)
+            continue;
         words[w] = strdup(tmp);
-        if(ui[a]==';'){
+        if (ui[a] == ';')
+        {
             w++;
             errorCode = interpreter(words, w);
-            if(errorCode == -1) return errorCode;
+            if (errorCode == -1)
+                return errorCode;
             a++;
             w = 0;
-            for(; ui[a]==' ' && a<1000; a++);        // skip white spaces
+            for (; ui[a] == ' ' && a < 1000; a++)
+                ; // skip white spaces
             continue;
         }
         w++;
-        a++; 
+        a++;
     }
     errorCode = interpreter(words, w);
     return errorCode;
