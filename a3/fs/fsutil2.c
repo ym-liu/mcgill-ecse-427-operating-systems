1d058528e7ef047aa4ed0cb2547d483413f4f91b
Darren Tao <tao.darren@gmail.com>
2024-04-03 03:25:18 -0400

    fix: fragmented

--- a3/fs/fsutil2.c
+++ a3/fs/fsutil2.c
@@ -184,9 +184,10 @@ int count_fragmentable_files()
 static bool is_file_fragmented(struct inode *inode)
 {
   block_sector_t prev_sector = -1;
-  for (int i = 0; i < bytes_to_sectors(inode_length(inode)); i++)
+  off_t file_size = inode_length(inode);
+  for (off_t i = 0; i < file_size; i += BLOCK_SECTOR_SIZE)
   {
-    block_sector_t sector = bytes_to_sectors(i * BLOCK_SECTOR_SIZE);
+    block_sector_t sector = bytes_to_sectors(i);
     if (prev_sector != -1 && (sector > prev_sector + 3 || sector < prev_sector))
     {
       return true;
