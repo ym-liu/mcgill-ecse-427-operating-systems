02befdde3da3651232e5f6fdb43956fdda7a03cb
Darren Tao <tao.darren@gmail.com>
2024-04-05 16:40:11 -0400

    fix: recover 2

--- a3/fs/fsutil2.c
+++ a3/fs/fsutil2.c
@@ -337,9 +337,13 @@ static void find_hidden_data_in_files()
   struct dir *dir = dir_open_root();
   struct dir_entry e;
 
-  while (dir_readdir(dir, e.name))
+  while (true)
   {
     block_sector_t inode_file = dir_readdir_inode(dir, e.name);
+    if (inode_file == -1)
+    {
+      break;
+    }
     struct inode *inode = inode_open(inode_file);
 
     if (inode == NULL || inode_is_directory(inode) || inode_is_removed(inode))
@@ -370,6 +374,7 @@ static void find_hidden_data_in_files()
         fputs(recovered, fp);
       }
       fclose(fp);
+      printf("%s\n", filename);
     }
   }
   dir_close(dir);
